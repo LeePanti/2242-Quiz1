29324cd6b75e589f9f07cbc95b04c6a9401fb8bd
Lee Panti <2019119651@ub.edu.bz>
2023-01-29 23:28:25 -0600

    triangle variable

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -16,4 +17,13 @@ func (t triangle) area() float64 {
 func (t triangle) perimeter() float64 {
 	hyp := math.Sqrt(math.Pow(t.base, 2) + math.Pow(t.height, 2))
 	return t.base + t.height + hyp
+}
+
+func main() {
+	myTriangle := triangle{
+		base:   3,
+		height: 4,
+	}
+	fmt.Println(myTriangle.area())
+	fmt.Println(myTriangle.perimeter())
 }
