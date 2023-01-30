c93f744c36da5c500f21c537267973c041ee004b
Lee Panti <2019119651@ub.edu.bz>
2023-01-29 23:34:05 -0600

    circle perimeter test

--- circle/circle_test.go
+++ circle/circle_test.go
@@ -13,3 +13,13 @@ func TestArea(t *testing.T) {
 		t.Errorf("Expected %v, got %v", want, got)
 	}
 }
+
+func TestPerimeter(t *testing.T) {
+	newCircle := circle{0.5}
+	want := 3.141592653589793
+	got := newCircle.perimeter()
+
+	if want != got {
+		t.Errorf("Expected %v, got %v", want, got)
+	}
+}
