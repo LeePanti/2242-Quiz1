e38315d4ae844d36920c66a063ffddc2e444dd70
Lee Panti <2019119651@ub.edu.bz>
2023-01-29 23:23:41 -0600

    triangle perimeter test

--- triangle/triangle_test.go
+++ triangle/triangle_test.go
@@ -13,3 +13,13 @@ func TestArea(t *testing.T) {
 		t.Errorf("Expected %v, got %v", want, got)
 	}
 }
+
+func TestPerimeter(t *testing.T) {
+	newTriangle := triangle{3, 4}
+	want := 12.00
+	got := newTriangle.perimeter()
+
+	if want != got {
+		t.Errorf("Expected %v, got %v", want, got)
+	}
+}
