be9c16832831307d35eb58b693874406666acfb1
Lee Panti <2019119651@ub.edu.bz>
2023-01-29 23:36:18 -0600

    circle type and methods

---
+++ circle/circle.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"math"
+)
+
+type circle struct {
+	radius float64
+}
+
+func (c circle) area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
+}
+
+func (c circle) perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
